68681cfa26bc1f9efd4664015612828a1c114c5e
Alena_Korma <alena.gorova93@gmail.com>
2025-03-09 00:05:55 +0200

    commit message

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import './App.css';
+import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBt';
+import SearchBar from './components/SearchBar/SearchBar';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import Loader from './components/Loader/Loader';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import ImageModal from './components/ImageModal/ImageModal';
+
+function App() {
+  
+
+  return (
+    <div>
+      <LoadMoreBtn/>
+      <SearchBar/>
+      <ImageGallery/>
+      <Loader/>
+      <ErrorMessage/>
+      <ImageModal/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ImageCard/ImageCard.jsx
@@ -0,0 +1,11 @@
+import css from './ImageCard.module.css';
+
+export default function ImageCard() {
+    return (
+    <div>
+  <img src="" alt="" />
+    </div> 
+    );
+}
+
+
